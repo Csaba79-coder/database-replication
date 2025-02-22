2041da945f9e52669ead7e521a93c7d641f881a7
Csaba Vadász <csabavadasz79@gmail.com>
2025-02-22 12:13:19 +0100

    Initial commit

---
+++ src/main/java/com/csaba79coder/databasereplication/DatabaseReplicationApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.databasereplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseReplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseReplicationApplication.class, args);
+    }
+
+}
